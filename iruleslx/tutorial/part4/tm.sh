074f18ccba636deb6c9e2926550ae000674ea23c
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-15 23:16:33 +0300

    Update tm.sh

--- iruleslx/tutorial/part4/tm.sh
+++ iruleslx/tutorial/part4/tm.sh
@@ -1,7 +1,17 @@
+# Display the number of restarts for the extension
 tmsh show ilx plugin ilxpi_DEMO extensions | grep Restart
+
+# Display the PID for the Node.js process running the extension
 tmsh show ilx plugin ilxpi_DEMO extensions | grep PID
+
+# Turn on verbose logging for SDMD and all the Node.js processes
 tmsh modify sys db log.sdmd.level value debug
-#tmsh modify sys db log.sdmd.level value info
+
+# Put the extension into debugging mode
 tmsh modify ilx plugin ilxpi_DEMO extensions { ilxex_DEMO { concurrency-mode single command-options add { --debug } } }
+
+# Find the debugging port assigned to the Node.js process
 tmsh show ilx plugin ilxpi_DEMO extensions | grep Debug
+
+# Start the Node Inpector for that port number
 /usr/lib/node_modules/.bin/node-inspector --web-host 10.100.113.50 --no-inject --debug-port 1038
