cf76eeb00e3b9346ef4714e98527e72e4ff6cd79
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-15 22:30:08 +0300

    Update tm.sh

--- iruleslx/tutorial/part4/tm.sh
+++ iruleslx/tutorial/part4/tm.sh
@@ -1,1 +1,7 @@
 tmsh show ilx plugin ilxpi_DEMO extensions | grep Restart
+tmsh show ilx plugin ilxpi_DEMO extensions | grep PID
+tmsh modify sys db log.sdmd.level value debug
+#tmsh modify sys db log.sdmd.level value info
+tmsh modify ilx plugin ilxpi_DEMO extensions { ilxex_DEMO { concurrency-mode single command-options add { --debug } } }
+tmsh show ilx plugin ilxpi_DEMO extensions | grep Debug
+/usr/lib/node_modules/.bin/node-inspector --web-host 10.100.113.50 --no-inject --debug-port 1038
