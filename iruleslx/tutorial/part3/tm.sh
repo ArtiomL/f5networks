0eeb5e098749dabac6902c353be2c34c9d4a39e8
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-15 20:22:36 +0300

    Update tm.sh

--- iruleslx/tutorial/part3/tm.sh
+++ iruleslx/tutorial/part3/tm.sh
@@ -1,3 +1,8 @@
+# Change the current directory to the extension folder
 cd /var/ilx/workspaces/Common/ilxws_DEMO/extensions/ilxex_DEMO/
+
+# Install the Node.js request module (https://www.npmjs.com/package/request)
 npm install request --save
+
+# Install the Node.js string module (https://www.npmjs.com/package/string)
 npm install string --save
