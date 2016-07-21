4a36026df270fd0155096200c88abe61edaa03c9
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-21 21:37:03 +0300

    Update irule_GEO_QUERY.tcl

--- iruleslx/tutorial/part3/irule_GEO_QUERY.tcl
+++ iruleslx/tutorial/part3/irule_GEO_QUERY.tcl
@@ -1,16 +1,16 @@
 #!iRule
-# F5 BIG-IP iRule: GeoLocation and IP Reputation Query
+# F5 Networks - iRule: GeoLocation and IP Reputation Query
+# https://github.com/ArtiomL/f5networks
 # Artiom Lichtenstein
 # v1.6m, 09/07/2016
-
+#
 # Usage:
 # http://VS_IP/geo?{IPv4_TO_TEST | IPv6_TO_TEST}
-
+#
 # Examples:
 # http://10.100.89.100/geo?8.8.8.8
 # http://10.100.89.100/geo?199.19.105.220
 # http://10.100.89.100/geo?2a02:DB8::45
-
 
 when HTTP_REQUEST {
 	if { [HTTP::uri] starts_with "/geo" } {
