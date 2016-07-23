0f01ed0f3fbbb89b489ea811455c4060ebcb403f
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-23 23:13:35 +0300

    Update tm.sh

--- iruleslx/tutorial/part5/tm.sh
+++ iruleslx/tutorial/part5/tm.sh
@@ -1,1 +1,14 @@
-#
+# Show all running Node.js processes
+ps -ef | grep [n]odejs
+
+# Change the current directory to the extension folder
+cd /var/ilx/workspaces/Common/ilxws_DEMO/extensions/ilxex_DEMO/
+
+# Show the current request module version
+npm list request
+
+# Check the registry to see if any installed packages are currently outdated
+npm outdated
+
+# Update the Node.js request module
+npm update request --save
