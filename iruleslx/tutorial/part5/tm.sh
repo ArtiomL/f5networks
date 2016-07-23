945282f226384e7ef0605501f414254636a73b0d
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-23 23:22:52 +0300

    Update tm.sh

--- iruleslx/tutorial/part5/tm.sh
+++ iruleslx/tutorial/part5/tm.sh
@@ -4,7 +4,7 @@ ps -ef | grep [n]odejs
 # Change the current directory to the extension folder
 cd /var/ilx/workspaces/Common/ilxws_DEMO/extensions/ilxex_DEMO/
 
-# Show the current request module version
+# Show the current Node.js request module version
 npm list request
 
 # Check the registry to see if any installed packages are currently outdated
