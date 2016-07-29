1fbc7ead09ec651997dc5804d2025e19313d956d
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-30 02:16:54 +0300

    Create arestime.sh

---
+++ shell/arestime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# F5 Networks - Auto-Restart NTPD
+# https://github.com/ArtiomL/f5networks
+# Artiom Lichtenstein
+# v1.2, 30/07/2016
+
+str_NTPD_STATUS=`tmsh show /sys service ntpd | awk '{ print $NF }' | cut -d'.' -f1`
+if [ "$str_NTPD_STATUS"  != "running" ]; then
+	str_TO_LOG=`tmsh restart /sys service ntpd`
+	logger -p local0.info "arestime.sh - $str_TO_LOG"
+fi
