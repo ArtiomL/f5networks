0fe8677311c119953542b2350e8c3b9deaa3da9d
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-08-02 22:32:08 +0300

    Update emon_AZURE_HA.py

--- azure/ha/emon_AZURE_HA.py
+++ azure/ha/emon_AZURE_HA.py
@@ -24,50 +24,54 @@ def funLog(intMesLevel, strMessage):
 class clsExCodes:
 	args = 4
 
-if len(sys.argv) < 4:
-	funLog(1, 'Not enough arguments, ILX / Node.js VS IP is missing?')
-	sys.exit(clsExCodes.args)
-
-# Remove IPv6/IPv4 compatibility prefix (LTM passes addresses in IPv6 format)
-strIP = sys.argv[1].strip(':f')
-strPort = sys.argv[2]
-# ILX / Node.js VS IP
-strIPNjs = sys.argv[3]
-# PID file
-strPFile = '_'.join(['/var/run/', os.path.basename(sys.argv[0]), strIP, strPort + '.pid'])
-# PID
-strPID = str(os.getpid())
-
-funLog(2, strPFile + ' ' + strPID)
-
-# Kill the last instance of this monitor if hung
-if os.path.isfile(strPFile):
+def main():
+	if len(sys.argv) < 4:
+		funLog(1, 'Not enough arguments, ILX / Node.js VS IP is missing?')
+		sys.exit(clsExCodes.args)
+	
+	# Remove IPv6/IPv4 compatibility prefix (LTM passes addresses in IPv6 format)
+	strIP = sys.argv[1].strip(':f')
+	strPort = sys.argv[2]
+	# ILX / Node.js VS IP
+	strIPNjs = sys.argv[3]
+	# PID file
+	strPFile = '_'.join(['/var/run/', os.path.basename(sys.argv[0]), strIP, strPort + '.pid'])
+	# PID
+	strPID = str(os.getpid())
+	
+	funLog(2, strPFile + ' ' + strPID)
+	
+	# Kill the last instance of this monitor if hung
+	if os.path.isfile(strPFile):
+		try:
+			os.kill(int(file(strPFile, 'r').read()), SIGKILL)
+			funLog(1, 'Killed the last hung instance of this monitor.')
+		except OSError:
+			pass
+	
+	# Record current PID
+	file(strPFile, 'w').write(str(os.getpid()))
+	
 	try:
-		os.kill(int(file(strPFile, 'r').read()), SIGKILL)
-		funLog(1, 'Killed the last hung instance of this monitor.')
-	except OSError:
-		pass
-
-# Record current PID
-file(strPFile, 'w').write(str(os.getpid()))
-
-try:
-	objResp = requests.head(''.join(['https://', strIP, ':', strPort]), verify = False)
-except requests.exceptions.RequestException as e:
-	funLog(1, str(e))
-	sys.exit(3)
-
-if objResp.status_code == 200:
-	os.unlink(strPFile)
-	# Any standard output stops the script from running. Clean up any temporary files before the standard output operation
-	print 'UP'
-	sys.exit()
-else:
-	try:
-		objResp = requests.head('http://' + strIPNjs)
+		objResp = requests.head(''.join(['https://', strIP, ':', strPort]), verify = False)
 	except requests.exceptions.RequestException as e:
-		# log e
-		sys.exit(2)
+		funLog(1, str(e))
+		sys.exit(3)
+	
+	if objResp.status_code == 200:
+		os.unlink(strPFile)
+		# Any standard output stops the script from running. Clean up any temporary files before the standard output operation
+		print 'UP'
+		sys.exit()
+	else:
+		try:
+			objResp = requests.head('http://' + strIPNjs)
+		except requests.exceptions.RequestException as e:
+			# log e
+			sys.exit(2)
+	
+	os.unlink(strPFile)
+	sys.exit(1)
 
-os.unlink(strPFile)
-sys.exit(1)
+if __name__ == '__main__':
+	main()
