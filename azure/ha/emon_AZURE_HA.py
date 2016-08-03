782d9da787f5b7fb0d717124b77d21b140d5f23d
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-08-04 02:35:25 +0300

    Update emon_AZURE_HA.py

--- azure/ha/emon_AZURE_HA.py
+++ azure/ha/emon_AZURE_HA.py
@@ -2,7 +2,7 @@
 # F5 Networks - External Monitor: Azure HA
 # https://github.com/ArtiomL/f5networks
 # Artiom Lichtenstein
-# v0.3, 02/08/2016
+# v0.4, 04/08/2016
 
 import json
 import os
@@ -13,7 +13,7 @@ import sys
 
 # Log level to /var/log/ltm
 intLogLevel = 2
-strLogID = '[-v0.3.160802-] emon_AZURE_HA.py - '
+strLogID = '[-v0.4.160804-] emon_AZURE_HA.py - '
 
 # Azure RM Auth
 strSubs = ''
@@ -28,7 +28,7 @@ strBearer = ''
 strLogger = 'logger -p local0.error '
 
 class clsExCodes:
-	args = 4
+	intArmAuth = 4
 
 def funLog(intMesLevel, strMessage):
 	if intLogLevel >= intMesLevel:
@@ -52,9 +52,6 @@ def funFailover():
 	funLog(1, 'Azure failover...')
 
 def main():
-	global strBearer
-	strBearer = funARMAuth()
-	funLog(2, 'ARM Bearer: ' + strBearer)
 	# Remove IPv6/IPv4 compatibility prefix (LTM passes addresses in IPv6 format)
 	strIP = sys.argv[1].strip(':f')
 	strPort = sys.argv[2]
@@ -62,9 +59,9 @@ def main():
 	strPFile = '_'.join(['/var/run/', os.path.basename(sys.argv[0]), strIP, strPort + '.pid'])
 	# PID
 	strPID = str(os.getpid())
-	
+
 	funLog(2, strPFile + ' ' + strPID)
-	
+
 	# Kill the last instance of this monitor if hung
 	if os.path.isfile(strPFile):
 		try:
@@ -72,10 +69,11 @@ def main():
 			funLog(1, 'Killed the last hung instance of this monitor.')
 		except OSError:
 			pass
-	
+
 	# Record current PID
 	file(strPFile, 'w').write(str(os.getpid()))
-	
+
+	# Health Monitor
 	try:
 		objResp = requests.head(''.join(['https://', strIP, ':', strPort]), verify = False)
 		if objResp.status_code == 200:
@@ -86,9 +84,18 @@ def main():
 			sys.exit()
 	except requests.exceptions.RequestException as e:
 		funLog(1, str(e))
-	
+
+	# Peer down, ARM action needed
+	global strBearer
+	strBearer = funARMAuth()
+	funLog(2, 'ARM Bearer: ' + strBearer)
+	if strBearer == 'BearERROR':
+		funLog(1, 'ARM Auth Error!')
+		sys.exit(clsExCodes.intArmAuth)
+
+	funCurState()
 	funFailover()
-	
+
 	os.unlink(strPFile)
 	sys.exit(1)
 
