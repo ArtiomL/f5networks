eb9dacc2781758a86c6d4c62b25035cbac170d3f
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-08-07 02:30:46 +0300

    Update emon_AZURE_HA.py

--- azure/ha/emon_AZURE_HA.py
+++ azure/ha/emon_AZURE_HA.py
@@ -2,30 +2,38 @@
 # F5 Networks - External Monitor: Azure HA
 # https://github.com/ArtiomL/f5networks
 # Artiom Lichtenstein
-# v0.5, 05/08/2016
+# v0.7, 07/08/2016
 
 import json
 import os
 import requests
 from signal import SIGKILL
+import socket
 from subprocess import call
 import sys
 
 # Log level to /var/log/ltm
 intLogLevel = 2
-strLogID = '[-v0.5.160805-] emon_AZURE_HA.py - '
+strLogID = '[-v0.7.160807-] emon_AZURE_HA.py - '
 
-# Azure RM REST Auth
-class clsAA:
-	strCFile = '/shared/tmp/azure/azure_ha.json'
-	strMgmtURI = 'https://management.azure.com/'
+# Azure RM REST API
+class clsAREA:
+	def __init__(self):
+		self.strCFile = '/shared/tmp/azure/azure_ha.json'
+		self.strMgmtURI = 'https://management.azure.com/'
+
+objAREA = clsAREA()
 
 # Logger command
 strLogger = 'logger -p local0.error '
 
 class clsExCodes:
-	intArgs = 8
-	intArmAuth = 4
+	def __init__(self):
+		intArgs = 8
+		intArmAuth = 4
+
+objExCodes = clsExCodes()
+
 
 def funLog(intMesLevel, strMessage):
 	if intLogLevel >= intMesLevel:
@@ -33,46 +41,56 @@ def funLog(intMesLevel, strMessage):
 		lstCmd.append(strLogID + strMessage)
 		call(lstCmd)
 
+
 def funARMAuth():
-	global clsAA
-	if not os.path.isfile(clsAA.strCFile):
-		funLog(1, 'Credentials file: %s is missing!' % clsAA.strCFile)
+	global objAREA
+	if not os.path.isfile(objAREA.strCFile):
+		funLog(1, 'Credentials file: %s is missing!' % objAREA.strCFile)
 		return 3
 
 	try:
-		with open(clsAA.strCFile, 'r') as f:
+		with open(objAREA.strCFile, 'r') as f:
 			diCreds = json.load(f)
-		clsAA.strSubID = diCreds['subID']
+		objAREA.strSubID = diCreds['subID']
 		strTenantID = diCreds['tenantID']
 		strAppID = diCreds['appID']
 		strPass = diCreds['pass']
 		strEndPt = 'https://login.microsoftonline.com/%s/oauth2/token' % strTenantID
 	except Exception as e:
-		funLog(1, 'Invalid credentials file: %s' % clsAA.strCFile)
+		funLog(1, 'Invalid credentials file: %s' % objAREA.strCFile)
 		return 2
 
-	objPayload = { 'grant_type': 'client_credentials', 'client_id': strAppID, 'client_secret': strPass, 'resource': clsAA.strMgmtURI }
+	objPayload = { 'grant_type': 'client_credentials', 'client_id': strAppID, 'client_secret': strPass, 'resource': objAREA.strMgmtURI }
 	try:
 		objAuthResp = requests.post(url=strEndPt, data=objPayload)
 		dicAJSON = json.loads(objAuthResp.content)
 		if 'access_token' in dicAJSON.keys():
-			clsAA.strBearer = dicAJSON['access_token']
+			objAREA.strBearer = dicAJSON['access_token']
 			return 0
 
 	except requests.exceptions.RequestException as e:
 		funLog(2, str(e))
 	return 1
 
+
+def funLocIP(strIP, intPort):
+	objUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	objUDP.connect((strIP, intPort))
+	return objUDP.getsockname()[0]
+
+
 def funCurState():
 	funLog(1, 'Current local state: ')
+
 
 def funFailover():
 	funLog(1, 'Azure failover...')
 
+
 def main():
 	if len(sys.argv) < 3:
 		funLog(1, 'Not enough arguments!')
-		sys.exit(clsExCodes.intArgs)
+		sys.exit(objExCodes.intArgs)
 
 	# Remove IPv6/IPv4 compatibility prefix (LTM passes addresses in IPv6 format)
 	strIP = sys.argv[1].strip(':f')
@@ -112,10 +130,10 @@ def main():
 	if funARMAuth() != 0:
 		funLog(1, 'ARM Auth Error!')
 		os.unlink(strPFile)
-		sys.exit(clsExCodes.intArmAuth)
+		sys.exit(objExCodes.intArmAuth)
 
 	# ARM Auth OK
-	funLog(2, 'ARM Bearer: %s' % clsAA.strBearer)
+	funLog(2, 'ARM Bearer: %s' % objAREA.strBearer)
 
 	funCurState()
 	funFailover()
