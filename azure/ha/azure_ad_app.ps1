4df9a25bf9157ab65856f089386eea4ff1c840d1
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-08-22 03:12:19 +0300

    Update azure_ad_app.ps1

--- azure/ha/azure_ad_app.ps1
+++ azure/ha/azure_ad_app.ps1
@@ -1,7 +1,7 @@
 # F5 Networks - Register Azure RM AD App for OAuth2 API Access
 # https://github.com/ArtiomL/f5networks
 # Artiom Lichtenstein
-# v1.1, 16/08/2016
+# v1.2, 21/08/2016
 
 # Login to Azure RM
 Login-AzureRmAccount
@@ -36,11 +36,15 @@ New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName 
 # Show all resource groups
 Get-AzureRmResourceGroup
 
-# Select the resource group where the Azure Load Balancer (LBAZ) resides
+# Select the resource group where the Azure Load Balancer (LBAZ), F5 VMs and their availability set reside
 $rgName = "rgPAPERSTSOAP"
+
+# Record F5 VMs' NICs
+$nicF5A = ((Get-AzureRmVM -ResourceGroupName $rgName -Name ((Get-AzureRmAvailabilitySet -ResourceGroupName $rgName).VirtualMachinesReferences[0].Id -split 'virtualMachines/')[1]).NetworkInterfaceIDs -split 'networkInterfaces/')[1]
+$nicF5B = ((Get-AzureRmVM -ResourceGroupName $rgName -Name ((Get-AzureRmAvailabilitySet -ResourceGroupName $rgName).VirtualMachinesReferences[1].Id -split 'virtualMachines/')[1]).NetworkInterfaceIDs -split 'networkInterfaces/')[1]
 
 # Encode the password
 $adaPass = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($adaPass))
 
-# JSON IDs
-@{ "subID" = $subsID; "tenantID" = $tenantID; "appID" = $appID; "pass" = $adaPass; "rgName" = $rgName } | ConvertTo-Json
+# JSON values
+@{ "subID" = $subsID; "tenantID" = $tenantID; "appID" = $appID; "pass" = $adaPass; "rgName" = $rgName; "nicF5A" = $nicF5A; "nicF5B" = $nicF5B } | ConvertTo-Json
