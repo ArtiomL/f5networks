8c6e1a8a06a04c7cc5b1f2062cf77e7ca3744ade
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-10-13 16:04:04 +0300

    Auto-Restart ntpd

--- shell/arestime.sh
+++ shell/arestime.sh
@@ -4,7 +4,7 @@
 # Artiom Lichtenstein
 # v1.2, 30/07/2016
 
-str_NTPD_STATUS=`tmsh show /sys service ntpd | awk '{ print $NF }' | cut -d'.' -f1`
+str_NTPD_STATUS=`tmsh show /sys service ntpd | awk '{print $NF}' | cut -d'.' -f1`
 if [ "$str_NTPD_STATUS"  != "running" ]; then
 	str_TO_LOG=`tmsh restart /sys service ntpd`
 	logger -p local0.info "arestime.sh - $str_TO_LOG"
