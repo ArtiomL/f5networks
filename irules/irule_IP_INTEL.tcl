2a3ab8b1639f0670ed765d61e3bc23c5c53df153
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-11-06 00:33:31 +0200

    IP Intelligence Enforcement

--- irules/irule_IP_INTEL.tcl
+++ irules/irule_IP_INTEL.tcl
@@ -1,1 +1,13 @@
 #!iRule
+# F5 Networks - iRule: IP Intelligence Enforcement
+# https://github.com/ArtiomL/f5networks
+# Artiom Lichtenstein
+# v1.1, 06/11/2016
+
+when CLIENT_ACCEPTED {
+	set lst_IP_REP [IP::reputation [IP::client_addr]]
+	if { [llength $lst_IP_REP] } {
+		drop
+		 #log local0.info "Dropped Connection from IP: [IP::client_addr] Reputation: $lst_IP_REP"
+	}
+}
