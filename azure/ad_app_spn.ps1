62052b8c01d025c51f1ea0094cdf872bd2f5ff1c
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2017-06-14 23:19:01 +0300

    Register Azure RM AD App for OAuth2 API Access

---
+++ azure/ad_app_spn.ps1
@@ -0,0 +1,36 @@
+# F5 Networks - Register Azure RM AD App for OAuth2 API Access
+# https://github.com/ArtiomL/f5networks
+# Artiom Lichtenstein
+# v1.4, 14/06/2017
+
+# Login to Azure RM
+Login-AzureRmAccount
+
+# Show all subscriptions
+Get-AzureRmSubscription
+
+# Select the subscription where the AD resides
+$adSub = Get-AzureRmSubscription -SubscriptionName "Paper Street Soap"
+$adSub | Select-AzureRmSubscription
+
+# Record IDs
+$tenantID = $adSub.TenantId
+
+# AD application password
+$adaPass = Read-Host 'Azure AD App Password:' -AsSecureString
+$adaPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($adaPass))
+
+# Create a new AAD application
+$armADApp = New-AzureRmADApplication -DisplayName "adappREST" -HomePage "https://paperstsoap.com/adapprest" -IdentifierUris "https://paperstsoap.com/adapprest" -Password $adaPass
+
+# Record the application ID
+$appID = $armADApp.ApplicationId.Guid
+
+# Create a new SPN
+New-AzureRmADServicePrincipal -ApplicationId $appID
+
+# Assign a new role
+New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $appID
+
+# Show values
+@{ "tenantID" = $tenantID; "clientID" = $appID; "servicePrincipalSecret" = $adaPass; }
